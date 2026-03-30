4557022dd5f711775198ea398dc8406b2e3db699
Lexlonnn <ravishrb8@gmail.com>
2026-03-31 02:18:05 +0530

    ada

--- app/profile/[name]/page.tsx
+++ app/profile/[name]/page.tsx
@@ -3,8 +3,24 @@ import { notFound, redirect } from "next/navigation";
 const LINKEDIN_MAP: Record<string, string> = {
   // Add or edit entries using: key -> https://www.linkedin.com/in/<profile>
   mathew: "https://www.linkedin.com/in/mathewjosephta/",
-  ravish: "https://www.linkedin.com/in/ravish-rb/",
-  charlie: "https://www.linkedin.com/in/charlie/",
+  anamikaumesh: "https://app.mulearn.org/profile/anamikaumesh@mulearn",
+  muhammedafreen: "https://www.linkedin.com/in/muhammed-afreen-260410330/",
+  devikaga: "https://app.mulearn.org/profile/devikaga-5@mulearn",
+  aishafathihah: "https://www.linkedin.com/in/aisha-fathiha-ab9669366/",
+  aleenageorge: "https://www.linkedin.com/in/aleena-george-0b1041291/",
+  afiyafathima: "https://www.linkedin.com/in/afiya-fathima/",
+  malavikarajan: "https://www.linkedin.com/in/malavika-rajan/",
+  harijithasokan: "https://app.mulearn.org/profile/harijithasokan@mulearn",
+  rbravish: "https://www.linkedin.com/in/ravish-rb/",
+  abhishekprabhakaran: "https://www.linkedin.com/in/abhishek-prabhakaran-733029291/",
+  josephsijothottakara: "https://app.mulearn.org/profile/josephsijo@mulearn",
+  kishankathik: "https://www.linkedin.com/in/kishan-karthik-282a93290/",
+  jeringeorge: "https://www.linkedin.com/in/jerin-george-468049291/",
+  anjalikrishna: "https://app.mulearn.org/profile/anjalikrishnam@mulearn",
+  asherantony: "https://www.linkedin.com/in/asher-antony-350051291/",
+  aaryathapr: "https://app.mulearn.org/profile/aaryathapr@mulearn",
+  mathewjosephta: "https://www.linkedin.com/in/mathewjosephta/",
+  anna: "https://www.linkedin.com/in/anna-joy-v-j-3b5049291/",
 };
 
 type ProfileRouteProps = {
