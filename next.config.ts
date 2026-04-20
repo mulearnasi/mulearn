4850fdca7af7b146f0e39fc2ea617197549c0157
mulearnasi <mulearnasi@gmail.com>
2026-04-20 10:57:54 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/newv",
+        destination: "https://forms.gle/your-google-form-link",
+        permanent: false, // keep false for forms
+      },
+    ];
+  },
 };
 
 export default nextConfig;
